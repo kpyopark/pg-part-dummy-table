570d54e2a8b5dba073c3bc36a883e0f45f1d80fb
kpyopark <toheavener01@gmail.com>
2022-01-07 21:06:17 +0900

    GZIP upload feature added.

--- jdbc-test/pg.test/src/main/java/com/elevenquest/test/App.java
+++ jdbc-test/pg.test/src/main/java/com/elevenquest/test/App.java
@@ -1,9 +1,16 @@
 package com.elevenquest.test;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
 import java.sql.*;
+import java.util.zip.GZIPInputStream;
 
 import org.postgresql.copy.CopyManager;
 import org.postgresql.core.BaseConnection;
@@ -16,16 +23,26 @@ public class App
 {
     // https://www.postgresql.org/message-id/1171970019.3101.328.camel%40coppola.muc.ecircle.de
     // https://stackoverflow.com/questions/1347646/postgres-error-on-insert-error-invalid-byte-sequence-for-encoding-utf8-0x0/45810272
-    static class RemoveNullCharacterReader extends FileReader
+    static class RemoveNullCharacterReader extends InputStreamReader
     {
         static int DEFAULT_BUFFER_SIZE = 65536;
 
-        public RemoveNullCharacterReader(File file) throws FileNotFoundException {
-            super(file);
+        public RemoveNullCharacterReader(InputStream in) {
+            super(in);
         }
-
-        public RemoveNullCharacterReader(String filepath) throws FileNotFoundException {
-            super(filepath);
+    
+        public RemoveNullCharacterReader(InputStream in, String charsetName)
+            throws UnsupportedEncodingException
+        {
+            super(in, charsetName);
+        }
+    
+        public RemoveNullCharacterReader(InputStream in, Charset cs) {
+            super(in, cs);
+        }
+    
+        public RemoveNullCharacterReader(InputStream in, CharsetDecoder dec) {
+            super(in, dec);
         }
 
         char[] orgbuffer = new char[DEFAULT_BUFFER_SIZE];
@@ -59,17 +76,22 @@ public class App
             Class.forName("org.postgresql.Driver");
             System.err.println("Connecting to " + args[0]);
             Connection con = DriverManager.getConnection(args[0],args[1],args[2]);
-
             System.err.println("Copying text data rows from stdin");
             long startime = System.currentTimeMillis();
             CopyManager copyManager = new CopyManager((BaseConnection) con);
-            FileReader fileReader = new RemoveNullCharacterReader(args[3]);
-            copyManager.copyIn("COPY tb_part_dummy_tsv FROM STDIN", fileReader );
+            // if you want to use gzip file. 
+            InputStreamReader isr = null;
+            boolean isGzipped = true;
+            if( isGzipped )
+                isr = new RemoveNullCharacterReader(new GZIPInputStream(new FileInputStream(args[3])));
+            else
+                isr = new RemoveNullCharacterReader(new FileInputStream(args[3]));
+            copyManager.copyIn("COPY tb_part_dummy_tsv FROM STDIN", isr );
             long finishtime = System.currentTimeMillis();
             long elapsedtime = finishtime - startime;
             System.out.println("elapsed time:" + (elapsedtime / 1000));
             System.err.println("Done.");
-            // con.commit();
+            isr.close();
         }
     }
 }
